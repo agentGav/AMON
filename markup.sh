e263a843b383071224694db50b68ca2d3ec8b636
Gavin Starks <gavin@dgen.net>
2021-03-16 10:26:37 +0000

    Update markup.sh

--- markup.sh
+++ markup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "---" > index.markdown
-echo "title: AMON Data Format " >> index.markdown
+echo "title: AMON Data Format" >> index.markdown
 echo "layout: default" >> index.markdown
 echo "---" >> index.markdown
 echo "" >> index.markdown
